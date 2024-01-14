97852c9500a296165a7e4fcc0aba1d076a9eb0e0
mkokabi <Mohsen@mokosolution.com>
2024-01-14 22:25:55 +1100

    Adding WorkOrder

--- src/root.component.tsx
+++ src/root.component.tsx
@@ -1,12 +1,14 @@
 import { FluentProvider, webLightTheme } from "@fluentui/react-components";
 import { ResizableColumns as Example } from "./components/example";
+import WorkOrders from "./components/WorkOrders";
 
 export default function Root(props) {
   return (
     <section>
       {props.name} is mounted!
       <FluentProvider theme={webLightTheme}>
-        <Example />
+        {/* <Example /> */}
+        <WorkOrders />
       </FluentProvider>
     </section>
   );
